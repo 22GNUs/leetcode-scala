a0b6176f99cddb8453f75500e4f60265ba2ec1a6
22GNUs <stormaroon@gmail.com>
2019-09-10 13:52:29 +0800

    解决Q9 palindrome-number

---
+++ src/main/scala/9.palindrome-number.scala
@@ -0,0 +1,20 @@
+/**
+  * 判断一个数字是否是回文
+  * 核心逻辑是: 只要判断后半部分跟前半部分是否相等就可以
+  * 比如: 1212 只需要判断12 == 12则可以确定是回文
+  * 如果是奇数, 则判断后半部分 / 10 与前半部分相等, 因为奇数中间位不需要判断
+  */
+object Q9 {
+  def isPalindrome(x: Int): Boolean = {
+    // 两种特殊情况
+    // 0 或者最后一位为0且不为0的数
+    if (x < 0 || (x % 10 == 0 && x != 0)) false
+    else {
+      def revert(s: Int, r: Int): (Int, Int) =
+        if (r >= s) (s, r) else revert(s / 10, r * 10 + s % 10)
+      // 提取出左半部分和右半部分
+      val (left, right) = revert(x, 0)
+      left == right || left == right / 10
+    }
+  }
+}
--- src/test/scala/SolutionTest.scala
+++ src/test/scala/SolutionTest.scala
@@ -51,6 +51,10 @@ class SolutionTest extends WordSpec with Matchers {
     multiAssert(Map(123 -> 321), Q7.reverse)
   }
 
+  "9.palindrome-number test case" in {
+    multiAssert(Map(121 -> true, 12321 -> true, 123 -> false), Q9.isPalindrome)
+  }
+
   private def multiAssert[K, V](kv: Map[K, V], f: K => V): Unit = {
     for ((a, s) <- kv) {
       val ret = f(a)
