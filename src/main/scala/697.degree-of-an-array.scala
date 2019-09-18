5d917ac19693c9079047dd4a91d437fd4c5b7c07
22GNUs <stormaroon@gmail.com>
2019-09-18 19:33:39 +0800

    临时提交

--- src/main/scala/697.degree-of-an-array.scala
+++ src/main/scala/697.degree-of-an-array.scala
@@ -1,5 +1,9 @@
 object Solution {
     def findShortestSubArray(nums: Array[Int]): Int = {
-        
-    }
+      val init = (List.empty[Int], 50000)       
+      nums.foldLeft(init) {
+        case ((Nil, _), item) => (List(item), 1)
+        case ((lst @ h :: _, size), item) =>
+          if (item != h) (List(item), 1) else (item +: lst, Math.min(size, lst.length + 1))
+    }._2
 }
