90d56af788a27c2fdee2b1aadd5c79df4aa7d90f
22GNUs <stormaroon@gmail.com>
2019-10-09 16:13:01 +0800

    Q226 resolved

---
+++ src/main/java/226.invert-binary-tree.java
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Q226 {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        TreeNode node = new TreeNode(root.val);
+        if (root.right != null) node.left = invertTree(root.right);
+        if (root.left != null) node.right = invertTree(root.left);
+        return node;
+    }
+}
