b0f723020ddba3c1d28638dfec8166bac3a891c9
22GNUs <stormaroon@gmail.com>
2019-10-09 17:59:33 +0800

    code format

--- src/main/scala/70.climbing-stairs.scala
+++ src/main/scala/70.climbing-stairs.scala
@@ -1,7 +1,6 @@
 object Q70 {
-    def climbStairs(n: Int): Int = {
-      if (n == 1) 1       
-      else if (n == 2) 2
-      else climbStairs(n - 1) + climbStairs(n - 2)
-    }
+  def climbStairs(n: Int): Int =
+    if (n == 1) 1
+    else if (n == 2) 2
+    else climbStairs(n - 1) + climbStairs(n - 2)
 }
