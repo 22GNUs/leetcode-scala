22f707bc067321fe06e28676d51c82c4f7bece96
22GNUs <stormaroon@gmail.com>
2019-10-09 17:39:33 +0800

    Q70 resolved

--- src/main/scala/70.climbing-stairs.scala
+++ src/main/scala/70.climbing-stairs.scala
@@ -1,5 +1,7 @@
-object Solution {
+object Q70 {
     def climbStairs(n: Int): Int = {
-        
+      if (n == 1) 1       
+      else if (n == 2) 2
+      else climbStairs(n - 1) + climbStairs(n - 2)
     }
 }
