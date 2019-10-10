6850355f9329bd1150ad61a68f34e6853b2d8af4
22GNUs <stormaroon@gmail.com>
2019-10-10 11:12:41 +0800

    Q70 memo solution

--- src/main/scala/70.climbing-stairs.scala
+++ src/main/scala/70.climbing-stairs.scala
@@ -1,6 +1,26 @@
+// 递归
 object Q70 {
   def climbStairs(n: Int): Int =
     if (n == 1) 1
     else if (n == 2) 2
     else climbStairs(n - 1) + climbStairs(n - 2)
+}
+
+// 递归 + 缓存
+object Q70Memo {
+  def climbStairs(n: Int): Int = {
+    var memo = Map.empty[Int, Int]
+    def loop: Int => Int = {
+      case 1 => 1
+      case 2 => 2
+      case n =>
+        if (memo.contains(n)) memo(n)
+        else {
+          val ret = loop(n - 1) + loop(n - 2)
+          memo += (n -> ret)
+          ret
+        }
+    }
+    loop(n)
+  }
 }
