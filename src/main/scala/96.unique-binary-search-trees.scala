12eec772422601403112614e9809f27da191d1b4
22GNUs <stormaroon@gmail.com>
2019-11-07 12:20:59 +0800

    add dp solution

--- src/main/scala/96.unique-binary-search-trees.scala
+++ src/main/scala/96.unique-binary-search-trees.scala
@@ -1,7 +1,7 @@
 /**
   * Q96递归
   * 每一轮把左边的子树个数乘以右边的子树个数
-  * 因为二差搜索树要求左边必须小于i, 右边必须大于i
+  * 因为二叉搜索树要求左边必须小于i, 右边必须大于i
   * 所以
   * 左边 -> i - 1
   * 右边 -> n - i
@@ -13,4 +13,21 @@ object Q96Rec {
       (1 to n).foldLeft(0) { (acc, item) =>
         acc + numTrees(item - 1) * numTrees(n - item)
       }
+}
+
+/**
+  * Q96 动态规划
+  */
+object Q96DP {
+  def numTrees(n: Int): Int = {
+    val dp = new Array[Int](n + 1)
+    dp(0) = 1
+    dp(1) = 1
+    (2 to n).foreach { i =>
+      (1 to i).foreach { j =>
+        dp(i) += dp(j - 1) * dp(i - j)
+      }
+    }
+    dp(n)
+  }
 }
