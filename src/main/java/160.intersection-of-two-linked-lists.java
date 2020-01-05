1c4abb543ff9029e90030ea21b9d3f90c76a85ed
stormaroon <stormaroon@gmail.com>
2020-01-05 21:15:43 +0800

    Q160

---
+++ src/main/java/160.intersection-of-two-linked-lists.java
@@ -0,0 +1,22 @@
+import java.common.ListNode;
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Q160 {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+      ListNode a = headA, b = headB;
+      while (a != b) {
+        a = a == null ? headB : a.next;
+        b = b == null ? headA : b.next;
+      }
+      return a;
+    }
+}
