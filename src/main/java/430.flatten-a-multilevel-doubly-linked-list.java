82152b2a318a9c7e43933696d4024401cff12dde
stormaroon <stormaroon@gmail.com>
2020-01-16 21:55:44 +0800

    code format

--- src/main/java/430.flatten-a-multilevel-doubly-linked-list.java
+++ src/main/java/430.flatten-a-multilevel-doubly-linked-list.java
@@ -10,29 +10,30 @@ class Node {
 };
 */
 class Q430 {
-    public Node flatten(Node head) {
-      Node curr = head;
-      while (curr != null) {
-        if (curr.child == null) {
-          curr = curr.next;
-          continue;
-        }
-        Node next = curr.next;
-        Node child = curr.child;
-
-        curr.next = child;
-        child.prev = curr;
-        // 注意点一
-        curr.child = null;
-
-        while (child.next != null) {
-          child = child.next;
-        }
-        child.next = next;
-        // 注意点二
-        if (next != null) next.prev = child;
+  public Node flatten(Node head) {
+    Node curr = head;
+    while (curr != null) {
+      if (curr.child == null) {
         curr = curr.next;
+        continue;
       }
-      return head;
+      Node next = curr.next;
+      Node child = curr.child;
+
+      curr.next = child;
+      child.prev = curr;
+      // 注意点一
+      curr.child = null;
+
+      while (child.next != null) {
+        child = child.next;
+      }
+      child.next = next;
+      // 注意点二
+      if (next != null)
+        next.prev = child;
+      curr = curr.next;
     }
+    return head;
+  }
 }
