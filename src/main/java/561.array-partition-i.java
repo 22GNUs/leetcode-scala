c24ce420d8c7689a0d0f6d9d781923d8e9ad6176
stormaroon <stormaroon@gmail.com>
2020-01-27 16:57:05 +0800

    Q561 resolved

---
+++ src/main/java/561.array-partition-i.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+class Q561 {
+  public int arrayPairSum(int[] nums) {
+    Arrays.sort(nums);
+    int sum = 0;
+    for (int i = 0; i < nums.length; i += 2) {
+      sum += Math.min(nums[i], nums[i + 1]);
+    }
+    return sum;
+  }
+}
