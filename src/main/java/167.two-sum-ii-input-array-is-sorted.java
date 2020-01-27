22f3379ab781123d2883b1604702ea3a5f0a5a38
stormaroon <stormaroon@gmail.com>
2020-01-27 17:14:48 +0800

    Q167 resolved

---
+++ src/main/java/167.two-sum-ii-input-array-is-sorted.java
@@ -0,0 +1,19 @@
+class Q167 {
+    public int[] twoSum(int[] numbers, int target) {
+      int i = 0, j = numbers.length - 1;       
+      while (i < numbers.length && j >= 0) {
+        int sum = numbers[i] + numbers[j];
+        if (sum == target) {
+          return new int[]{i + 1, j + 1};
+        }
+        // 太大了, 把大的挪小
+        if (sum > target) {
+          j--;
+        } else {
+        // 太小了, 把小的挪大
+          i++;
+        }
+      }
+      return new int[]{-1, -1};
+    }
+}
