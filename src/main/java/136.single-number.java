30fc301aff19bb13e090167695d6ee2bdec689e1
stormaroon <stormaroon@gmail.com>
2020-01-28 20:51:27 +0800

    Q136 resolved

---
+++ src/main/java/136.single-number.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+import java.util.Map;
+class Q136 {
+  public int singleNumber(int[] nums) {
+    int ans = 0;
+    for (int i = 0; i < nums.length; i++) {
+      // 0跟任何数亦或都为0
+      // 两个相同的数亦或也为0
+      // 亦或满足交换率, 因此相同的数都两两亦或为0最后只剩不重复的数
+      ans ^= nums[i];
+    }
+    return ans;
+  }
+}
---
+++ src/main/java/217.contains-duplicate.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+  public boolean containsDuplicate(int[] nums) {
+    Set<Integer> set = new HashSet<>();
+    for (int i = 0; i < nums.length; i++) {
+      if (set.contains(nums[i])) {
+        return true;
+      }
+      set.add(nums[i]);
+    }
+    return false;
+  }
+}
