17cd3a0a71d343c48bb40f9e8d18583df3999903
stormaroon <stormaroon@gmail.com>
2020-03-08 15:42:58 +0800

    Q146 resolved

---
+++ src/main/java/146.lru-cache.java
@@ -0,0 +1,29 @@
+class LRUCache extends LinkedHashMap<Integer, Integer> {
+
+  private final int capacity;
+
+  public LRUCache(int capacity) {
+    // 这个true很重要, 表示accessingOrder根据读取排序
+    super(capacity, 0.75F, true);
+    this.capacity = capacity;
+  }
+
+  public int get(int key) {
+    Integer v = super.get(key);
+    return v == null ? -1 : v;
+  }
+
+  public void put(int key, int value) {
+    super.put(key, value);
+  }
+
+  @Override
+  protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+    return size() > capacity;
+  }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such: LRUCache obj =
+ * new LRUCache(capacity); int param_1 = obj.get(key); obj.put(key,value);
+ */
